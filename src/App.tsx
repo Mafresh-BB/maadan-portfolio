5410c3ac58a0f750994f5508c17d629982eef3b0
Abdulyekeen Babatunde <108339356+Mafresh-BB@users.noreply.github.com>
2025-12-21 07:25:20 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ export default function App() {
       <CallToAction scrollToContact={() => scrollToSection('contact')} />
       <Contact />
       <Footer />
-
+ 
       <AIChatWidget />
       <BackToTop />
       
